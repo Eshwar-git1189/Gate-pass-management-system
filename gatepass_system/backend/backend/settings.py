2fdcf5186d2ee778a82a3628ae8216c598dda0e3
Eshwar K S <eshwarks1234@gmail.com>
2025-10-03 16:07:49 +0530

    backend setup

--- gatepass_system/backend/backend/settings.py
+++ gatepass_system/backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gatepass',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
--- gatepass_system/backend/backend/urls.py
+++ gatepass_system/backend/backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from gatepass.views import StudentViewSet, GatepassRequestViewSet
+
+router = DefaultRouter()
+router.register(r'students', StudentViewSet)
+router.register(r'gatepasses', GatepassRequestViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
+
